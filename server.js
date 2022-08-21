3a902d8ad01e1902b385e5e2ef03c45f9776f590
prosper collins <prospercollins19@gmail.com>
2022-08-21 05:04:33 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server,{cors:{origin:'*'}});
+
+
+app.set('view engine', 'ejs');
+
+app.get('/home', (req,res)=>{
+
+res.render('home');
+
+});
+
+
+server.listen(3001, ()=>{
+
+  console.log("server running");
+});
+
+ 
+
+io.on('connection',(socket)=>{
+
+    console.log("User connected " + socket.id); 
+
+    socket.on('disconnect',()=>{
+           console.log('User disconnected');
+    });
+
+    socket.on("message", (data) =>{
+
+        socket.broadcast.emit("message", data);
+    });
+
+});
